fcfef5b68209de1c5e7bb0fac502f8116159dc0e
kmor8 <kamilaoo88@outlook.com>
2023-09-05 21:44:13 -0500

    Se crea el servidor

---
+++ src/main/java/com/smartcoach/smartcoachBackend/SmartcoachBackendApplication.java
@@ -0,0 +1,13 @@
+package com.smartcoach.smartcoachBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartcoachBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartcoachBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/smartcoach/smartcoachBackend/SmartcoachBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smartcoach.smartcoachBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartcoachBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
